97311ff46a0e9e26569ab2a81950bc41b7b81be5
Oksana-Botsieva <bocievaoks@yandex.ru>
2024-05-03 14:36:24 +0400

    - popup modal window - phone number output to log

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// добавить решение
+document.addEventListener('DOMContentLoaded', () => {
+
+    const moreBtn = document.querySelector('.more');
+    const overlay = document.querySelector('.overlay');
+    const closeBtn = document.querySelector('.popup-close');
+
+
+    moreBtn.addEventListener('click', () => {
+        overlay.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    });
+
+    closeBtn.addEventListener('click', () => {
+        overlay.style.display = 'none';
+        document.body.style.overflow = '';
+    });
+
+    const popupBtn = document.querySelector('.popup-form__btn');
+
+    popupBtn.addEventListener('click', () => {
+        const phoneNumber = document.querySelector('.popup-form__input').value;
+        console.log('Номер телефона:', phoneNumber);
+    })
+})
